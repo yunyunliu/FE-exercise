0dfe9e491a3625469d5ecbda585c5347fd05912c
Yunyun Liu <celeph4is@gmail.com>
2022-02-03 12:49:37 -0800

    set up react

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// import UserForm from './UserForm';
+
+ReactDom.render (
+  <div>testtesttest</div>,
+  document.getElementById('root')
+);
