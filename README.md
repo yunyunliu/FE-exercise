f5a3f131cd12f02ef6711b8ef2929f3c57e3081f
Yunyun Liu <celeph4is@gmail.com>
2022-02-05 17:52:54 -0800

    add README and comments

---
+++ README.md
@@ -0,0 +1,33 @@
+# User Creation Form
+
+
+## Technologies Used
+
+- React.js
+- HTML5
+- CSS3
+- Webpack
+- Babel
+
+### Getting Started
+
+1. Clone the repository.
+
+ ```shell
+    git clone https://github.com/yunyunliu/FE-exercise.git
+    cd FE-exercise
+  ```
+
+2. Install all dependencies with NPM.
+
+ ```shell
+    npm install
+ ```
+
+3. Build with webpack
+
+ ```shell
+    npm run build
+ ```
+
+4. Open `index.html`
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -13,12 +13,12 @@ const UserForm = () => {
   const [state, setState] = useState('');
 
   const [missing, setMissing] = useState([]);
-  const [view, setView] = useState('success');
+  const [view, setView] = useState('user-form');
   const [errMessage, setErrMessage] = useState('');
 
   const url = 'https://frontend-take-home.fetchrewards.com/form';
 
-  useEffect(() => { // GET request to get data needed for populating state and occupation dropdowns
+  useEffect(() => { // API call to get data for populating dropdown options
     fetch(url)
       .then(res => res.json())
       .then(options => {setOptions(options)})
@@ -27,35 +27,29 @@ const UserForm = () => {
 
   const handleSubmit = async e => {
     e.preventDefault(); // prevent page reload
-    if (name && email && password && occupation && state) { // if fields all filled, send post request
-      setMissing([]); // clear missing fields so Notification component is not displayed
-      const res = await fetch(url, {
+    if (name && email && password && occupation && state) { // validate data -> check for any fields left blank
+      setMissing([]); // clear missing fields to make sure Notification component is not displayed
+      const res = await fetch(url, { // make POST request
         method: 'POST',
         body: JSON.stringify({ name, email, password, occupation, state }),
         headers: { 'Content-Type': 'application/json' }
       });
-      if (res.ok) {
-        // setErrMessage('');
-        // setName(''); // clear form fields
-        // setEmail('');
-        // setPassword('');
-        // setOccupation('');
-        // setState('');
+      if (res.ok) { // if POST request is successful, show success message
         setView('success')
-      } else {
+      } else { // otherwise set error message to be displayed
         setErrMessage('An error occurred. Try again later.');
       }
-    } else {
-      const missing = []; // contains names of fields left empty by user; is passed into Notification component
+    } else { // missing data
+      const missing = [];
       const userData = { name, email, password, occupation, state };
-      for (const field in userData) {
+      for (const field in userData) { // find which fields were left blank; push them into an array
         if (!userData[field]) {
           missing.push(field);
         }
       }
-      setMissing(missing);
+      setMissing(missing); // set as state, so it can be passed into Notification component
     }
-  }
+  };
 
   const ErrorMessage = () => (
     <div className='notification'>
@@ -68,72 +62,71 @@ const UserForm = () => {
       <div>
         <p>Successfully created new user.</p>
       </div>
-    )
-  } else {
-    return (
-      <form onSubmit={e => handleSubmit(e)}>
-        <h2>Sign-Up</h2>
-        { missing.length > 0
-            ? <Notification missing={missing} />
-            : null }
-        { errMessage ? ErrorMessage() : null }
-        <label htmlFor='name'> Full Name <span>*</span></label>
-        <input
-          id='name'
-          className='gray-border'
-          placeholder='Name'
-          value={name}
-          onChange={({ target }) => {setName(target.value)}}
-        />
-        <label htmlFor='email'> Email <span>*</span></label>
-        <input
-          id='email'
-          // type='email'
-          className='gray-border'
-          placeholder='Email'
-          value={email}
-          onChange={({ target }) => {setEmail(target.value)}}
-        />
-        <label htmlFor='pass'> Password  <span>*</span></label>
-        <input
-          id='pass'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={({ target }) => {setPassword(target.value)}}
-        />
-        <label htmlFor='occupation'> Occupation <span>*</span></label>
-        <select
-          id='occupation'
-          value={occupation}
-          onChange={({ target }) => {setOccupation(target.value)}}
-        >
-          <option value=''>Select Occupation</option>
-            { options
-              ? options.occupations.map((occ, i) => (
-                <option key={i} value={occ}>{occ}</option>
-              ))
-              : null
-            }
-        </select>
-        <label htmlFor='state'>State <span>*</span></label>
-        <select
-          id='state'
-          value={state}
-          onChange={({ target }) => { setState(target.value)}}
-        >
-          <option value=''>Select State</option>
-            { options
-              ? options.states.map(({ name, abbreviation }) => (
-                <option key={abbreviation} value={abbreviation}>{name}</option>
-              ))
-              : null
-            }
-        </select>
-        <button className='btn-submit'> Create Account</button>
-      </form>
     );
   }
+  return (
+    <form onSubmit={e => handleSubmit(e)}>
+      <h2>Sign-Up</h2>
+      { missing.length > 0
+          ? <Notification missing={missing} />
+          : null }
+      { errMessage ? ErrorMessage() : null }
+      <label htmlFor='name'> Full Name <span>*</span></label>
+      <input
+        id='name'
+        className='gray-border'
+        placeholder='Name'
+        value={name}
+        onChange={({ target }) => {setName(target.value)}}
+      />
+      <label htmlFor='email'> Email <span>*</span></label>
+      <input
+        id='email'
+        // type='email'
+        className='gray-border'
+        placeholder='Email'
+        value={email}
+        onChange={({ target }) => {setEmail(target.value)}}
+      />
+      <label htmlFor='pass'> Password  <span>*</span></label>
+      <input
+        id='pass'
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={({ target }) => {setPassword(target.value)}}
+      />
+      <label htmlFor='occupation'> Occupation <span>*</span></label>
+      <select
+        id='occupation'
+        value={occupation}
+        onChange={({ target }) => {setOccupation(target.value)}}
+      >
+        <option value=''>Select Occupation</option>
+          { options
+            ? options.occupations.map((occ, i) => (
+              <option key={i} value={occ}>{occ}</option>
+            ))
+            : null
+          }
+      </select>
+      <label htmlFor='state'>State <span>*</span></label>
+      <select
+        id='state'
+        value={state}
+        onChange={({ target }) => { setState(target.value)}}
+      >
+        <option value=''>Select State</option>
+          { options
+            ? options.states.map(({ name, abbreviation }) => (
+              <option key={abbreviation} value={abbreviation}>{name}</option>
+            ))
+            : null
+          }
+      </select>
+      <button className='btn-submit'> Create Account</button>
+    </form>
+  );
 };
 
 export default UserForm;
